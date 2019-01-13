f7d03bbf939cdcf6147d99fcef6aca9b6b787c41
christophe.seguinot <christophe.seguinot@gmail.com>
2019-01-13 16:29:22 +0100

    logos, notification, sw

--- src/components/ToolbarActions/index.jsx
+++ src/components/ToolbarActions/index.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import UserLinks from "../UserLinks";
 import "./ToolbarActions.scss";
+import { withStyles } from '@material-ui/core/styles';
+import NotificationButton from '../NotificationButton';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
+
 
 class Toolbar extends Component {
   render() {
     const { config } = this.props;
     return (
       <div className="toolbar-actions">
+        <NotificationButton/>
         <div className="userlinks-container">
           <UserLinks config={config} />
         </div>
