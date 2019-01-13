b31c73291400ab2134e81322cecb2fd81f04f020
christophe.seguinot <christophe.seguinot@gmail.com>
2019-01-13 16:50:27 +0100

    test navigator existence

--- src/components/NotificationButton/index.jsx
+++ src/components/NotificationButton/index.jsx
@@ -22,47 +22,52 @@ const styles = theme => ({
 function  displayConfirmNotification () {
   console.log('displayConfirmationNotification');
   console.log('coucou2');
-  if ('serviceWorker' in navigator) {
-    console.log('serviceWorker in navigator');
-    var options = {
-      body: 'You successfully subscribed to our Notification service!',
-      icon: '/logos/logo-192.png',
-      image: '/logos/logo-192.png',
-      dir: 'ltr',
-      lang: 'fr-FR', // BCP 47,
-      vibrate: [100, 50, 200],
-      badge: '/logos/logo-192.png',
-      tag: 'confirm-notification',
-      renotify: true,
-    };
-    console.log('coucou 1');
-    console.log(navigator.serviceWorker);
-    navigator.serviceWorker.ready
-      .then(function(swreg) {
-        console.log('coucou');
-        console.log(swreg);
-        console.log('serviceWorkerRegistration retrieved', swreg);
-        swreg.showNotification('Successfully subscribed (from SW)!', options);
-        console.log('notification sent');
-      }).catch(function(err) {
-        console.log(err);
-      });
+  if(typeof navigator !== 'undefined') {
+    if ('serviceWorker' in navigator) {
+      console.log('serviceWorker in navigator');
+      var options = {
+        body: 'You successfully subscribed to our Notification service!',
+        icon: '/logos/logo-192.png',
+        image: '/logos/logo-192.png',
+        dir: 'ltr',
+        lang: 'fr-FR', // BCP 47,
+        vibrate: [100, 50, 200],
+        badge: '/logos/logo-192.png',
+        tag: 'confirm-notification',
+        renotify: true,
+      };
+      console.log('coucou 1');
+      console.log(navigator.serviceWorker);
+      navigator.serviceWorker.ready
+        .then(function(swreg) {
+          console.log('coucou');
+          console.log(swreg);
+          console.log('serviceWorkerRegistration retrieved', swreg);
+          swreg.showNotification('Successfully subscribed (from SW)!', options);
+          console.log('notification sent');
+        }).catch(function(err) {
+          console.log(err);
+        });
+    }
   }
 }
 
 class NotificationButton extends Component {
     constructor(props) {
-      if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-          .register('/sw.js')
-          .then(function () {
-            console.log('Service worker registered!');
-          })
-          .catch(function(err) {
-            console.log(err);
-          });
-      }
       super(props);
+      if(typeof navigator !== 'undefined') {
+        if ('serviceWorker' in navigator) {
+          navigator.serviceWorker
+            .register('/sw.js')
+            .then(function () {
+              console.log('Service worker registered!');
+            })
+            .catch(function(err) {
+              console.log(err);
+            });
+        }
+      }
+      
     }
 
   
