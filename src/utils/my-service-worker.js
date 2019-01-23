d91929b7993ef2e56dce16031ba13ab5de27dccf
christophe.seguinot <christophe.seguinot@gmail.com>
2019-01-23 10:39:29 +0100

    fix mode offline

--- src/utils/my-service-worker.js
+++ src/utils/my-service-worker.js
@@ -1,26 +1,13 @@
 
-// Required stuff
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js'
-  )
+  importScripts("workbox-v3.6.3/workbox-sw.js");
+  workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
+
+  workbox.skipWaiting();
+  workbox.clientsClaim();
+
   
   // The plugin will pass the files to cache here
   workbox.precaching.precacheAndRoute([])
-
-  console.log('coucou');  
-// self.addEventListener('install', function(event) {
-//     console.log('[Service Worker] Installing Service Worker ...', event);
-//   });
-  
-//   self.addEventListener('activate', function(event) {
-//     console.log('[Service Worker] Activating Service Worker ...', event);
-//     return self.clients.claim();
-//   });
-  
-//   self.addEventListener('fetch', function(event) {
-//     console.log('[Service Worker] Fetching something ...', event);
-//     event.respondWith(fetch(event.request));
-//   });
 
   self.addEventListener('notificationclick', function(event) {
     var notification = event.notification;
