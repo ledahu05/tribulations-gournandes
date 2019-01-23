89e9ebeece20b2fd1dd65ff031cb73ccbdf91fcc
christophe.seguinot <christophe.seguinot@gmail.com>
2019-01-23 11:24:34 +0100

    fixing custom sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,19 +1,5 @@
-console.log('coucou');
-
-self.addEventListener('install', function(event) {
-    console.log('[Service Worker] Installing Service Worker ...', event);
-  });
-  
-  self.addEventListener('activate', function(event) {
-    console.log('[Service Worker] Activating Service Worker ...', event);
-    return self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    console.log('[Service Worker] Fetching something ...', event);
-    event.respondWith(fetch(event.request));
-  });
-
+ console.log('custom sw loaded');
+ 
   self.addEventListener('notificationclick', function(event) {
     var notification = event.notification;
     var action = event.action;
