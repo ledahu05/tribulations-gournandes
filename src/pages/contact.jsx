345e84d6b9bee938a9fa5570e5de1946c6ec4806
ledahu <christophe.seguinot@gmail.com>
2019-09-16 18:06:05 +0200

    retry md

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -36,24 +36,39 @@ const contact = props => {
                   data-netlify-honeypot="bot-field"
               >
                   <input type="hidden" name="bot-field" />
-                  <div>
-                      <label htmlFor="name">Name</label>
-                      <input type="text" name="name" id="name" />
+                  <TextField
+                      id="name"
+                      label="Nom"
+                      rows={2}
+                      maxRows={6}
+                      lineDirection="center"
+                      placeholder="Merci de saisir votre nom"
+                      className="md-cell md-cell--12"
+                  />
+                  <TextField
+                      id="email"
+                      label="Email"
+                      rows={2}
+                      maxRows={6}
+                      lineDirection="center"
+                      placeholder="Merci de saisir votre adresse email"
+                      className="md-cell md-cell--12"
+                  />
+                  <TextField
+                      id="message"
+                      label="Message"
+                      rows={2}
+                      maxRows={6}
+                      placeholder="Merci de saisir votre message"
+                      className="md-cell md-cell--12"
+                  />
+                  <CardActions className="md-cell md-cell--12">
+                      <Button raised primary type="submit" className="md-cell--right">
+                          Envoyer
+            </Button>
+                  </CardActions>
+                  </form>
                   </div>
-                  <div>
-                      <label htmlFor="email">Email</label>
-                      <input type="text" name="email" id="email" />
-                  </div>
-                  <div>
-                      <label htmlFor="message">Message</label>
-                      <textarea name="message" id="message" rows="6" required />
-                  </div>
-                  <div>
-                      <input type="submit" value="Drop a line" />
-                      <input type="reset" value="Eraser" />
-                  </div>
-              </form>
-      </div>
     </Layout>
   );
 };
